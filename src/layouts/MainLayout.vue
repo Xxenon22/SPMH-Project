075651d4d2920881c89d1449c0308fc41039a200
Efraim Munthe <efrayanglain@gmail.com>
2024-06-11 15:00:10 +0700

    make new mainlayout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import BaseLayout from '@/layouts/BaseLayout.vue'
+</script>
+
+<template>
+  <BaseLayout>
+    <main class="max-w-7xl mx-auto p-4">
+      <slot />
+    </main>
+  </BaseLayout>
+</template>
