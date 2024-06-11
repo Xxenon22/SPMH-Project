0e783cccf7231c7ab79c349b0ce6a16d8ea4f6bb
Efraim Munthe <efrayanglain@gmail.com>
2024-06-11 10:15:31 +0700

    add type param to function

--- src/composables/menu.ts
+++ src/composables/menu.ts
@@ -6,8 +6,13 @@ export function useMenu() {
   const menu = ref<Menu[] | null>([])
   const menuItem = ref<Menu | null>(null)
 
-  async function getMenu(page: number = 1) {
-    const { count, data, err } = await useFetch(`menu?page=${page}`)
+  async function getMenu(page: number = 1, type?: string) {
+    let url = `menu?page=${page}`
+    if (type) {
+      url += `&type_id=${type}`
+    }
+
+    const { count, data, err } = await useFetch(url)
     if (err) throw err
     return { count, data }
   }
