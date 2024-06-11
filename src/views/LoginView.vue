dfcbd16208cef189a1c9380ba8d6a37ab40aa03c
Efraim Munthe <efrayanglain@gmail.com>
2024-06-11 14:34:36 +0700

    implement half circle

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,25 +5,25 @@ import BaseLayout from '../layouts/BaseLayout.vue'
 
 <template>
   <BaseLayout>
-    <div class="grid">
-      <img src="../assets/fluent_drink-coffee-16-filled.png" alt="" />
-
+    <div class="grid grid-cols-1 min-h-screen">
       <form
-        class="bg-primary max-w-8xl mx-auto p-8 rounded-lg flex flex-col gap-4"
+        class="bg-primary max-w-8xl mx-auto p-8 rounded-lg flex flex-col gap-4 place-self-center relative"
       >
-        <h1 class="text-center">Login</h1>
+        <figure
+          class="bg-primary flex justify-center items-center p-16 rounded-full absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        >
+          <img
+            src="../assets/fluent_drink-coffee-16-filled.png"
+            class="size-16"
+            width="50"
+            height="50"
+            alt=""
+          />
+        </figure>
+        <h1 class="text-center z-10">Login</h1>
         <TextInput placeholder="Username" />
         <TextInput placeholder="Password" />
-        <span
-          ><input
-            type="checkbox"
-            name="checkbox"
-            id="checkbox"
-            class="checkbox"
-          />
-          Remember this account</span
-        >
-        <button class="btn bg-accent mx-auto">Login</button>
+        <button class="btn bg-accent mx-auto w-full shadow-lg">Login</button>
 
         <div class="go-SignUp">
           <span>Don’t have an account? <a href="#">make account</a></span>
@@ -34,67 +34,8 @@ import BaseLayout from '../layouts/BaseLayout.vue'
 </template>
 
 <style>
-img {
-  margin-left: 40%;
-  width: 112px;
-  border: 1px solid #b88554;
-  border-radius: 100px 100px;
-  background-color: #b88554;
-  padding: 10px 10px;
-}
-
-.container {
-  border-radius: 20px;
-}
-
-.container h1 {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 40px;
-  font-family: 'Jacques Francois';
-  border: 1px solid #b88554;
-  color: #fff;
-}
-
-.container span {
-  font-size: 15px;
-  line-height: 20px;
-  letter-spacing: 0.3px;
-  margin: 20px 0;
-}
-
-.container a {
-  font-size: 20px;
-}
-
-form button {
-  margin-left: 30%;
-  background-color: #654326;
-  color: #fff;
-  font-size: 30px;
-  border-radius: 20px;
-  width: 150px;
-  padding: 5px;
-  font-weight: 500;
-  letter-spacing: 0.5px;
-}
-
-.container button {
-  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.35);
-}
-
-span input {
-  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.35);
-}
-
-.form-container {
-  position: absolute;
-}
-
 .go-SignUp {
   margin: 30px;
   white-space: noWrap;
 }
 </style>
-
