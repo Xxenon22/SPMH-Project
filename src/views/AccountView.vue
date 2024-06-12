e26eb2ad9aa74a4eea7d2c0fb2a5142d744177bb
Efraim Munthe <efrayanglain@gmail.com>
2024-06-12 11:18:29 +0700

    display account details

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import { useAuthStore } from '@/stores/auth'
 import BaseLayout from '../layouts/BaseLayout.vue'
+
+const authStore = useAuthStore()
 </script>
 
 <template>
   <BaseLayout>
-    <h1>Account</h1>
+    <h1>{{ authStore.user.name }}</h1>
+    <p>{{ authStore.user.email }}</p>
   </BaseLayout>
 </template>
